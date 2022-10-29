504acfd9d5d394e3b7e3bd25908c584ffcf28785
waleedbensghaier <107326748+waleedbensghaier@users.noreply.github.com>
2022-10-29 22:14:41 +0100

    Add files via upload

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -27,7 +27,7 @@ import tensorflow.keras.layers as KL
 KE = KL
 import tensorflow.keras.models as KM
 
-from mrcnn_demo import utils
+from mrcnn import utils
 
 # Requires TensorFlow 1.3+ and Keras 2.0.8+.
 from distutils.version import LooseVersion
